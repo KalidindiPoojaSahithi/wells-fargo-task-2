090b634fc13482686e3bdbedb2522380f2ee5ead
KalidindiPoojaSahithi <85690283+KalidindiPoojaSahithi@users.noreply.github.com>
2024-05-19 13:35:14 -0400

    Update Entrypoint.java

--- src/main/java/com/wellsfargo/counselor/Entrypoint.java
+++ src/main/java/com/wellsfargo/counselor/Entrypoint.java
@@ -1,6 +1,7 @@
 package com.wellsfargo.counselor;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
